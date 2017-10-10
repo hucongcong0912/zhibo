ba539bae18a5c02157abd26003b66c10bcb76df6
505561427 <baoyuyu7474741@qq.com>
2017-10-10 09:20:34 +0800

    样式更改
    用户个人中心

--- laravel54/resources/views/index/user.blade.php
+++ laravel54/resources/views/index/user.blade.php
@@ -5,63 +5,59 @@
 @stop()
     {{-- 用户中心 --}}
     <div class="upload">
-        <?php echo "<pre>"; var_dump($user); echo "</pre>"; ?>
+        <?php /*echo "<pre>"; var_dump($user); echo "</pre>";*/ ?>
             <!-- container -->
             <div class="container">
                 <div class="upload-grids">
                     <div class="upload-right">
                         <div class="upload-file">
-                            <div class="services-icon">
+                            <!-- <div class="services-icon">
                                 <span class="glyphicon glyphicon-open" aria-hidden="true"></span>
-                            </div>
+                            </div> -->
+                            <center>
+                                <?php 
+                                 if ($user->img == '') {
+                                     $img = "./images/check.jpg";
+                                 }else{
+                                     $img = $user->img;
+                                 } 
+                                ?>
+                                <img src="<?= $img ?>" alt="头像">
+                            </center>
                             <input type="file" value="Choose file..">
                         </div>
                         <div class="upload-info">
-                            <h5>Select files to upload</h5>
-                            <span>or</span>
-                            <p>Drag and drop files</p>
+                            <h5><?= $user->nickname ?></h5>
+                            <span>个性签名</span>
+                            <p><?= $user->sign ?></p>
                         </div>
                     </div>
                     <div class="upload-right-bottom-grids">
-                        <div class="col-md-4 upload-right-bottom-left">
-                            <h4>Help and Suggestions</h4>
+                        <div class="upload-right-bottom-left">
+                            <h4>基本信息</h4>
+                            <?php
+                             if ($user->sex==1) {$sex = '男';}else{$sex = '女';}
+                             if ($user->is_anchor==1) {$is_anchor = '是';}else{$is_anchor = '否';}
+                            ?>
                             <div class="upload-right-top-list">
                                 <ul>
-                                    <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                                    <li><a href="#">Fusce egestas tincidunt massa</a></li>
-                                    <li><a href="#">Pellentesque vehicula quis tellus</a></li>
-                                    <li><a href="#">Etiam gravida feugiat tortor eget eleifend</a></li>
-                                    <li><a href="#">Etiam iaculis facilisis metus a viverra</a></li>
-                                    <li><a href="#">Fusce sed enim non orci molestie</a></li>
-                                    <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+                                    <li><span>账号ID：</span><a href="#"><?= $user->user_id ?></a></li>
+                                    <li><span>性别：</span><a href="#"><?= $sex ?></a></li>
+                                    <li><span>生日：</span><a href="#"><?= $user->happy ?></a></li>
+                                    <li><span>手机：</span><a href="#"><?= $user->tel ?></a></li>
+                                    <li><span>邮箱：</span><a href="#"><?= $user->email ?></a></li>
                                 </ul>
                             </div>
                         </div>
-                        <div class="col-md-4 upload-right-bottom-left">
-                            <h4>Import videos</h4>
+                        <div class="upload-right-bottom-left">
+                            <h4>账号信息</h4>
                             <div class="upload-right-top-list">
                                 <ul>
-                                    <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                                    <li><a href="#">Fusce egestas tincidunt massa</a></li>
-                                    <li><a href="#">Pellentesque vehicula quis tellus</a></li>
-                                    <li><a href="#">Etiam gravida feugiat tortor eget eleifend</a></li>
-                                    <li><a href="#">Etiam iaculis facilisis metus a viverra</a></li>
-                                    <li><a href="#">Fusce sed enim non orci molestie</a></li>
-                                    <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-md-4 upload-right-bottom-left">
-                            <h4>Live streaming</h4>
-                            <div class="upload-right-top-list">
-                                <ul>
-                                    <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                                    <li><a href="#">Fusce egestas tincidunt massa</a></li>
-                                    <li><a href="#">Pellentesque vehicula quis tellus</a></li>
-                                    <li><a href="#">Etiam gravida feugiat tortor eget eleifend</a></li>
-                                    <li><a href="#">Etiam iaculis facilisis metus a viverra</a></li>
-                                    <li><a href="#">Fusce sed enim non orci molestie</a></li>
-                                    <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+                                    <li><span>等级：</span><a href="#"><?= $user->grade ?></a></li>
+                                    <li><span>经验：</span><a href="#"><?= $user->experience ?></a></li>
+                                    <li><span>爵位：</span><a href="#"><?= $user->rank ?></a></li>
+                                    <li><span>财产：</span><a href="#"><?= $user->prpent ?></a></li>
+                                    <li><span>主播：</span><a href="#"><?= $is_anchor ?></a></li>
                                 </ul>
                             </div>
                         </div>
